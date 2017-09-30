3c403c786ad7e6889fea1217348835408957f9b3
Maxim Stegnienko <Stegnienko46125@mail.ru>
2017-09-30 16:22:57 +0300

    Update UI

--- TinkoffChat/TinkoffChat/AppDelegate.swift
+++ TinkoffChat/TinkoffChat/AppDelegate.swift
@@ -7,65 +7,39 @@
 //
 
 import UIKit
-import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var oldStateOfApplicationValue: Int = UIApplication.shared.applicationState.rawValue
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        print ("Application moved from start to \(nameOfState(withValue: application.applicationState.rawValue)) state: \(#function)" )
+      
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
    
-        print ("Application moved from \(nameOfState(withValue: oldStateOfApplicationValue)) to \(nameOfState(withValue: application.applicationState.rawValue)) state: \(#function)" )
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-     
-        print ("Application moved from \(nameOfState(withValue: oldStateOfApplicationValue)) to \(nameOfState(withValue: application.applicationState.rawValue)) state: \(#function)" )
+  
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
      
-        print ("Application moved from \(nameOfState(withValue: oldStateOfApplicationValue)) to \(nameOfState(withValue: application.applicationState.rawValue)) state: \(#function)" )
+     
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        
-        print ("Application moved from \(nameOfState(withValue: oldStateOfApplicationValue)) to \(nameOfState(withValue: application.applicationState.rawValue)) state: \(#function)" )
+  
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
       
-        print ("Application moved from \(nameOfState(withValue: oldStateOfApplicationValue)) to suspended state: \(#function)" )
-    }
-  
-    
-    func nameOfState(withValue value:Int) -> String {
-        
-        if value != oldStateOfApplicationValue {
-           
-            oldStateOfApplicationValue = value
-        
-        }
-      
-        switch value {
-        case 0:
-            return "active"
-        case 1:
-            return "inactive"
-        case 2:
-            return "background"
-        default: break
-        }
-        return ""
+   
     }
     
 }
--- TinkoffChat/TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/TinkoffChat/ProfileViewController.swift
@@ -8,82 +8,163 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
 
-  
+class ProfileViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
+
+    
+    @IBOutlet weak var editButtonlabel: UIButton!
+    @IBOutlet weak var selectProfilePhotoButtonLabel: UIButton!
+    @IBOutlet weak var placeholderPhotoProfile: UIImageView!
+    
+
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        if self.editButtonlabel != nil {
+            if let frame = self.editButtonlabel?.frame {
+                print (frame)
+            }
+        } else {
+            print ("editButtonlabel no exist")
+        }
+
+        /*
+             Проблема возникла в результате того что самой кнопки как объекта еще не существует в момент работы init, соответсвенно
+             нельзя получить frame того чего нет. p.s. что мертво умереть не может )))
+        */
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-         print("State is \(UIApplication.shared.nameOfState()) and method is \(#function)")
         
+        if self.editButtonlabel != nil {
+            if let frame = self.editButtonlabel?.frame {
+                 print (frame)
+            }
+        } else {
+            print ("editButtonlabel no exist")
+        }
+
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
-         print("State is \(UIApplication.shared.nameOfState()) and method is \(#function)")
-
-  
+      super.viewWillAppear(animated)
+   
+        if self.editButtonlabel != nil {
+            if let frame = self.editButtonlabel?.frame {
+                print (frame)
+            }
+        } else {
+            print ("editButtonlabel no exist")
+        }
+        
+         /*
+             Здесь frame не отличается от того что в viewDidLoad
+         */
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-       
-        print("State is \(UIApplication.shared.nameOfState()) and method is \(#function)")
-
         
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-       
-        print("State is \(UIApplication.shared.nameOfState()) and method is \(#function)")
-
+        super.viewDidAppear(animated)
+        
+        if self.editButtonlabel != nil {
+            if let frame = self.editButtonlabel?.frame {
+                print (frame)
+            }
+        } else {
+            print ("editButtonlabel no exist")
+        }
+        
+        /*
+             После наложения constrait к моменту viewDidAppear происходит изменение frame у editbutton
+             Обращаю внимание что если запустить на iphone 5 или SE, то frame будет одинаковый так как весь перерасчет идет относительно того что находиться в interface builder
+        */
+        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+       
+        self.editButtonlabel.layer.borderWidth = 1.5
+        self.editButtonlabel.layer.cornerRadius = self.editButtonlabel.bounds.height/4
+        self.placeholderPhotoProfile.layer.masksToBounds = true
+        self.selectProfilePhotoButtonLabel.layer.masksToBounds = true
+        self.selectProfilePhotoButtonLabel.layer.cornerRadius = self.selectProfilePhotoButtonLabel.bounds.height/2
+        self.placeholderPhotoProfile.layer.cornerRadius = self.selectProfilePhotoButtonLabel.layer.cornerRadius
+        self.selectProfilePhotoButtonLabel.layer.cornerRadius = self.selectProfilePhotoButtonLabel.bounds.height/2
+        self.placeholderPhotoProfile.layer.cornerRadius = self.selectProfilePhotoButtonLabel.layer.cornerRadius
+       
+        let offsetOfButton = self.selectProfilePhotoButtonLabel.bounds.height/5
+        self.selectProfilePhotoButtonLabel.imageEdgeInsets = UIEdgeInsetsMake(offsetOfButton, offsetOfButton, offsetOfButton, offsetOfButton)
         
-         print("State is \(UIApplication.shared.nameOfState()) and method is \(#function)")
-
-    
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    
+    // MARK - Actions
+    
+    @IBAction func editButtonAction(_ sender: UIButton) {
         
-        print("State is \(UIApplication.shared.nameOfState()) and method is \(#function)")
-
         
     }
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
+    @IBAction func selectProfilePhotoAction(_ sender: Any) {
         
-         print("State is \(UIApplication.shared.nameOfState()) and method is \(#function)")
-
-    
+        print ("Choose image profile")
+        
+        let alertViewController = UIAlertController(title: "Change Profile Photo", message: nil, preferredStyle: .actionSheet)
+        alertViewController.addAction(UIAlertAction(title: "Choose From Library", style: .`default`, handler:  {[weak self] action in
+            
+            self?.imagePickerWith(sourceType: .photoLibrary)
+            
+        }))
+        alertViewController.addAction(UIAlertAction(title: "Take Photo", style: .`default`, handler: {[weak self] action in
+            
+            self?.imagePickerWith(sourceType: .camera)
+            
+        }))
+        alertViewController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: {
+            (alertAction: UIAlertAction!) in
+            alertViewController.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alertViewController, animated: true, completion: nil)
     }
-
     
-}
-
-extension UIApplication {
-    
-    func nameOfState() -> String {
-        
-        switch UIApplication.shared.applicationState.rawValue {
-        case 0:
-            return "active"
-        case 1:
-            return "inactive"
-        case 2:
-            return "background"
-        default: break
+  @objc func alertControllerBackgroundTapped(_ recognizer: UITapGestureRecognizer)
+    {
+        if(recognizer.state == UIGestureRecognizerState.ended){
+            self.dismiss(animated: true, completion: nil)
         }
-        return ""
+   
+    }
+
+    func imagePickerWith(sourceType: UIImagePickerControllerSourceType) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        self.present(imagePicker, animated: true, completion: nil)
     }
     
+    
+    // MARK - UIImagePickerControllerDelegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.placeholderPhotoProfile.contentMode = .scaleAspectFill
+            self.placeholderPhotoProfile.image = pickedImage
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
 }
+
+
 
